2634171a514a75d8e1f27757cde2afea6bbff0ba
arturradiuk <arturradiuk@gmail.com>
2021-03-07 19:51:48 +0200

    mvn set up for the project api

---
+++ api/src/main/java/first_task/api/App.java
@@ -0,0 +1,13 @@
+package first_task.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
