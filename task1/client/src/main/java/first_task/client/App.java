bb57ffc6c361465d412b2d3b8be058c6782d4a76
arturradiuk <arturradiuk@gmail.com>
2021-03-08 22:31:09 +0200

    profiles & properties

--- task1/client/src/main/java/first_task/client/App.java
+++ task1/client/src/main/java/first_task/client/App.java
@@ -2,29 +2,50 @@ package first_task.client;
 
 import first_task.api.Person;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        final String email = "bob@example.com";
-        final String name = "Bob";
-        final String password = "123456";
-        final Integer age = 19;
+// remember to execute clean after changing active env
+public class App {
+
+    public Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+        return prop;
+    }
+
+    public static void main(String[] args) throws IOException {
+//        final String email = "bob@example.com";
+//        final String name = "Bob";
+//        final String password = "123456";
+//        final Integer age = 19;
+
+
+        String mode = new App().loadPropertiesFile("properties-from-pom.properties").getProperty("env");
+
+        String resFile = "config." + mode + ".properties";
+
+        Properties properties = new App().loadPropertiesFile(resFile);
+
+        final String email = properties.getProperty("email");
+        final String name = properties.getProperty("name");
+        final String password = properties.getProperty("password");
+        final Integer age = Integer.parseInt(properties.getProperty("age"));
 
         Person person = new Person();
         person.setEmail(email);
         person.setName(name);
         person.setPassword(password);
         person.setAge(age);
-
         System.out.println(person.toString());
+
 
     }
 }
